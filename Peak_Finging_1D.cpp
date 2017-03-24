f18d9a39c72f6bcd9daff9b9c5015c7c3d65b6d2
chytsa <hk416103@gmai.com>
2017-03-24 15:38:26 +0800

    modified:   Peak_Finging_1D.cpp 	.gitignore

--- Peak_Finging_1D.cpp
+++ Peak_Finging_1D.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int A[7] = {1, 6, 4, 3, 12, 19, 18};
-	int i = 0, j = 6;
+	int *A;
+	int n;
+	cout << "Please enter the size of array: ";
+	cin >> n;
+	A = new int[n];
+	cout << "Please input the elements:\n";
+	for(int k = 0; k < n; k++){
+		cin >> A[k];
+	}
+	int i = n, j = 0;
 	int mid;
 	
-	while(j - i > 1){
+	while(i - j > 1){
 		mid = (i + j)/2;
-		if(A[mid] >= A[mid - 1]){
+		if(A[mid] >= A[mid + 1]){
 			//cout << "A\n";
 			i = mid;
 		}
 		else{
 			//cout << "B\n";
-			j = mid - 1;
+			j = mid + 1;
 		}
 	}
 	//cout << i << "," << j << endl;
